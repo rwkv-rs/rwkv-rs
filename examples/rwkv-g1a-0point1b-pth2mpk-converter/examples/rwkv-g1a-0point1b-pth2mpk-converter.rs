5491f4f5fd3372a660a5a6f60ee48da7e664da2f
涟Ripple. <18930215187@163.com>
2025-11-01 02:28:29 +0800

    refactor(examples/rwkv-g1a-0point1b-pth2mpk-converter): rename

--- examples/rwkv-g1a-0point1b-pth2mpk-converter/examples/rwkv-g1a-0point1b-pth2mpk-converter.rs
+++ examples/rwkv-g1a-0point1b-pth2mpk-converter/examples/rwkv-g1a-0point1b-pth2mpk-converter.rs
@@ -4,8 +4,8 @@ use rwkv_lm::auto_regressive_model::AutoRegressiveModelConfig;
 
 fn main() {
     type MyBackend = Cuda<bf16, i32>;
-    let model_path = "./weights/rwkv7-g1-0.1b-20250307-ctx4096.pth";
-    let output_path = "./weights/rwkv7-g1-0.1b-20250307-ctx4096.mpk";
+    let model_path = "../weights/rwkv7-g1a-0.1b-20250728-ctx4096.pth";
+    let output_path = "../weights/rwkv7-g1a-0.1b-20250728-ctx4096.mpk";
     let model_config = AutoRegressiveModelConfig::new(12, 65536, 768, 12, 64);
     let option = ConvertPthToMpkOptions::new(model_path, output_path);
     convert_pth_to_mpk::<MyBackend>(&option, model_config);
